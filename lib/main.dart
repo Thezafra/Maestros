8bb59388c9ac279dc7e0a938727dc345f3908246
Paulo Aqueveque <138402386+Thezafra@users.noreply.github.com>
2026-01-23 17:57:35 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomeScreen,
+      home: const HomeScreen(),
     );
   }
 }
